ca6fd1c2300e132d20dd10f82f72a40b6208a85c
aqua.wang <aqua.wang@ucloud.cn>
2017-07-14 12:16:31 +0800

    modify default time to UTC+0800 for Asia/Shanghai timezone.

--- workflow/process.py
+++ workflow/process.py
@@ -2,7 +2,13 @@
 
 import alfred
 import calendar
+import time
+import math
 from delorean import utcnow, parse, epoch
+
+# global var to see if user
+# has a time offset
+_offset = 0
 
 def process(query_str):
     """ Entry point """
@@ -16,23 +22,95 @@ def parse_query_value(query_str):
     """ Return value for the query string """
     try:
         query_str = str(query_str).strip('"\' ')
-        if query_str == 'now':
-            d = utcnow()
+
+        if not query_str.startswith('utc'):
+            global _offset
+            # get correction for local time and DST
+            _offset = local_time_offset()
+            # get current time
+            epoch_time = time.time()
+            # create delorean with local time 
+            d = epoch(float(epoch_time+_offset)) 
+
         else:
-            # Parse datetime string or timestamp
-            try:
-                d = epoch(float(query_str))
-            except ValueError:
-                d = parse(str(query_str))
-    except (TypeError, ValueError):
+            if query_str.endswith('now'):
+                d = utcnow() 
+            else:
+                # Parse datetime string or timestamp
+                try:
+                    d = epoch(float(query_str[4:]))
+                except ValueError:
+                    d = parse(str(query_str[4:]))
+    except (TypeError, ValueError) as e:
+        print e
         d = None
     return d
+
+def local_time_offset(t=None):
+    """Return offset of local zone from GMT, either at present or at time t."""
+    # python2.3 localtime() can't take None
+    if t is None:
+        t = time.time()
+
+    if time.localtime(t).tm_isdst and time.daylight:
+        return -time.altzone
+    else:
+        return -time.timezone
+
+def construct_offset_string():
+    global _offset
+
+    # round to the nearest minute by adding 30 seconds
+    # then take the floor of 60, then multiply again by 60
+    # to get back to seconds, just in case
+    _offset = int((_offset + 30) // 60) * 60 
+
+    # convert to hours, round to 1 dp
+    _offset = round((float(float(_offset) / float(3600))),1)
+    
+    # save the sign
+    # using a ternary-like conditional
+    # added in Python 2.5 http://cloud.zoooot.com/OCLx
+    # so all Alfred 2 users should be on 2.5+
+    sign = "-" if _offset < 0 else "+"
+
+    # check for half hour timezones
+    if math.fmod(_offset, 1) != 0:
+        # convert FLOAT to string
+        offsetStr = str(math.fabs(_offset))
+    else:
+        # convert to INT then a string
+        offsetStr = str(int(abs(_offset)))
+
+    #if length is 1, we add a zero in front and 2 after
+    if len(offsetStr) == 1:
+        offsetStr = "0" + offsetStr + ":00"
+    elif len(offsetStr) == 2:
+        #else just 2 after - still a whole hour
+        offsetStr = offsetStr + ":00"
+    elif len(offsetStr) == 3:
+        #half hour single digit
+        # reuse the offset
+        offsetStr = "0" + str(int(abs(_offset))) + ":30"
+    elif len(offsetStr) == 4:
+        #half hour double digit
+        offsetStr = str(int(abs(_offset))) + ":30"
+
+    # add the sign back on
+    offsetStr = sign + offsetStr
+
+    return offsetStr
 
 def alfred_items_for_value(value):
     """
     Given a delorean datetime object, return a list of
     alfred items for each of the results
     """
+    subtitleStr=u'UTC Timestamp'
+
+    if _offset != 0:
+        tmpStr = construct_offset_string()
+        subtitleStr=u'UTC' + tmpStr + u' Timestamp'
 
     index = 0
     results = []
@@ -41,7 +119,7 @@ def alfred_items_for_value(value):
     item_value = calendar.timegm(value.datetime.utctimetuple())
     results.append(alfred.Item(
         title=str(item_value),
-        subtitle=u'UTC Timestamp',
+        subtitle=subtitleStr,
         attributes={
             'uid': alfred.uid(index), 
             'arg': item_value,
@@ -84,3 +162,4 @@ if __name__ == "__main__":
     except IndexError:
         query_str = None
     process(query_str)
+    
